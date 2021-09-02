895f811a9663ca6694ba26a9746c76e2f7aa6c65
Ricardo Perez <rp352429@gmail.com>
2021-09-02 01:32:17 -0400

    change api url

--- src/configuration.js
+++ src/configuration.js
@@ -1,6 +1,6 @@
 
 let configuration = {
-	api_url : process.env.API_URL || 'http://localhost:8888/api/v1'
+	api_url : 'https://crud-node-rp.herokuapp.com/api/v1' || 'http://localhost:8888/api/v1'
 }
 
 export default configuration;
